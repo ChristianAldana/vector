44727ab0c6ae6b58138f4ec4accb57c6ba7eccc7
Christian <caldanas2@miumg.edu.gt>
2022-05-14 11:14:08 -0600

    EJERCICIO 100 DESCENDETE

---
+++ ejerciciosVectores.java
@@ -0,0 +1,17 @@
+
+package ejerciciosvectores;
+
+import java.util.Scanner;
+
+public class ejerciciosVectores {
+
+    public static void main(String[] args) {
+
+            Scanner entrada = new Scanner (System.in);
+            
+            vectorSuma ejerciciosVectores  = new vectorSuma (); //ESCANER VECTOR
+		
+            ejerciciosVectores.cienNumerosVector(); //LLAMAR AL VECTOR 
+}
+    
+}
---
+++ vectorSuma.java
@@ -0,0 +1,15 @@
+
+package ejerciciosvectores;
+
+public class vectorSuma {
+      
+    public void  cienNumerosVector (){
+    int cienvectores [] = new int [100];
+    for (int i=0; i<cienvectores.length; i++){
+    
+        cienvectores[i] = i + 1;
+     System.out.println(cienvectores [i]);
+}
+    
+    } 
+}
